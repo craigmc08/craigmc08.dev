a3299cb54587ee60d43cc7928f4ed90ca258940c
craigmc08 <craigmc08@gmail.com>
2019-03-07 16:38:08 -0500

    Add box shadow to project details back button animation

--- src/project-details.js
+++ src/project-details.js
@@ -2,7 +2,6 @@ import anime from 'animejs';
 
 const backButt = document.querySelector('.back-butt');
 if (backButt) backButt.addEventListener('click', () => {
-    // window.history.back();
     const outer = document.querySelector('.outer');
     if (outer) {
         outer.style.position = 'fixed';
@@ -11,6 +10,7 @@ if (backButt) backButt.addEventListener('click', () => {
         outer.style.width = '100%';
         outer.style.height = '100%';
         outer.style.transform = 'translateX(0)';
+        outer.style.boxShadow = '0 5px 25px rgba(0, 0, 0, 0.3)';
         anime({
             targets: [outer],
             translateX: '100%',
