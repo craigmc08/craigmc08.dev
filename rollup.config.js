55c0307d1fe8276287b35c659808fbcc4d3ef6cc
craigmc08 <craigmc08@gmail.com>
2019-08-19 22:13:44 -0400

    Adds a single combined build script that runs the build in one process

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 // glob pattern input file matching from
 // https://github.com/rollup/rollup/issues/1435#issue-233487806
 
-import globby from 'globby';
+const globby = require('globby');
 
-import nodeResolve from 'rollup-plugin-node-resolve';
+const nodeResolve = require('rollup-plugin-node-resolve');
 
-export default globby.sync('src/*.js').map(inputFile => ({
+module.exports = globby.sync('src/*.js').map(inputFile => ({
     input: inputFile,
     output: {
         file: inputFile.replace('src', 'dist/js'),
